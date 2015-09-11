6658a7298987a33b250f814cdf90e091727f17a8
Robin Bowes <robin.bowes@yo61.com>
2015-09-11 15:38:49 +0100

    Forgot to set file ownership in new config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,15 +4,20 @@ class logrotate::config{
   $manage_cron_daily = $::logrotate::manage_cron_daily
   $config            = $::logrotate::config
 
-  file {'/etc/logrotate.d':
-      ensure => directory,
-      mode   => '0755',
+  file{'/etc/logrotate.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode   => '0755',
   }
+
   if $manage_cron_daily {
-    file {'/etc/cron.daily/logrotate':
-        ensure => file,
-        mode   => '0555',
-        source => 'puppet:///modules/logrotate/etc/cron.daily/logrotate',
+    file{'/etc/cron.daily/logrotate':
+      ensure => file,
+      owner => 'root',
+      group => 'root',
+      mode   => '0555',
+      source => 'puppet:///modules/logrotate/etc/cron.daily/logrotate',
     }
   }
 
