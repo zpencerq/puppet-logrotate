5fdf585cf7eccdf730d2e10ed39f1932cd66f6a8
Robin Bowes <rbowes@williamhill.co.uk>
2015-12-13 14:14:12 +0000

    Use osfamily fact in the defaults spec test to match the code

--- spec/classes/defaults_spec.rb
+++ spec/classes/defaults_spec.rb
@@ -7,7 +7,7 @@ describe '::logrotate::defaults' do
         let(:facts) do
           facts
         end
-        case facts[:operatingsystem]
+        case facts[:osfamily]
         when 'Debian'
           it do
             should contain_logrotate__rule('wtmp').with({
@@ -28,6 +28,32 @@ describe '::logrotate::defaults' do
               'create_owner' => 'root',
               'create_group' => 'utmp',
               'missingok'    => true,
+            })
+          end
+        when 'RedHat'
+          it do
+            should contain_logrotate__rule('wtmp').with({
+              'path'         => '/var/log/wtmp',
+              'create_mode'  => '0664',
+              'missingok'    => false,
+              'minsize'      => '1M',
+              'rotate_every' => 'month',
+              'create'       => true,
+              'create_owner' => 'root',
+              'create_group' => 'utmp',
+              'rotate'       => '1',
+            })
+
+            should contain_logrotate__rule('btmp').with({
+              'path'         => '/var/log/btmp',
+              'create_mode'  => '0600',
+              'minsize'      => '1M',
+              'missingok'    => true,
+              'rotate_every' => 'month',
+              'create'       => true,
+              'create_owner' => 'root',
+              'create_group' => 'utmp',
+              'rotate'       => '1',
             })
           end
         end
