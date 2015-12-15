61d59ff1e55e3dd10914bf353743fc7eff2e04fa
Robin Bowes <rbowes@williamhill.co.uk>
2015-12-15 23:48:10 +0000

    Add compress, delaycompress, ifempty to defaults tests

--- spec/classes/defaults_spec.rb
+++ spec/classes/defaults_spec.rb
@@ -11,49 +11,64 @@ describe '::logrotate::defaults' do
         when 'Debian'
           it do
             should contain_logrotate__rule('wtmp').with({
-              'rotate_every' => 'month',
-              'rotate'       => '1',
-              'create'       => true,
-              'create_mode'  => '0664',
-              'create_owner' => 'root',
-              'create_group' => 'utmp',
-              'missingok'    => true,
+              'rotate_every'  => 'month',
+              'rotate'        => '1',
+              'create'        => true,
+              'create_mode'   => '0664',
+              'create_owner'  => 'root',
+              'create_group'  => 'utmp',
+              'missingok'     => true,
+              'compress'      => 'undef',
+              'delaycompress' => 'undef',
+              'ifempty'       => 'undef',
             })
+          end
 
+          it do
             should contain_logrotate__rule('btmp').with({
-              'rotate_every' => 'month',
-              'rotate'       => '1',
-              'create'       => true,
-              'create_mode'  => '0600',
-              'create_owner' => 'root',
-              'create_group' => 'utmp',
-              'missingok'    => true,
+              'rotate_every'  => 'month',
+              'rotate'        => '1',
+              'create'        => true,
+              'create_mode'   => '0600',
+              'create_owner'  => 'root',
+              'create_group'  => 'utmp',
+              'missingok'     => true,
+              'compress'      => 'undef',
+              'delaycompress' => 'undef',
+              'ifempty'       => 'undef',
             })
           end
         when 'RedHat'
           it do
             should contain_logrotate__rule('wtmp').with({
-              'path'         => '/var/log/wtmp',
-              'create_mode'  => '0664',
-              'missingok'    => false,
-              'minsize'      => '1M',
-              'rotate_every' => 'month',
-              'create'       => true,
-              'create_owner' => 'root',
-              'create_group' => 'utmp',
-              'rotate'       => '1',
+              'path'          => '/var/log/wtmp',
+              'create_mode'   => '0664',
+              'missingok'     => false,
+              'minsize'       => '1M',
+              'rotate_every'  => 'month',
+              'create'        => true,
+              'create_owner'  => 'root',
+              'create_group'  => 'utmp',
+              'rotate'        => '1',
+              'compress'      => 'undef',
+              'delaycompress' => 'undef',
+              'ifempty'       => 'undef',
             })
+          end
 
+          it do
             should contain_logrotate__rule('btmp').with({
-              'path'         => '/var/log/btmp',
-              'create_mode'  => '0600',
-              'minsize'      => '1M',
-              'missingok'    => true,
-              'rotate_every' => 'month',
-              'create'       => true,
-              'create_owner' => 'root',
-              'create_group' => 'utmp',
-              'rotate'       => '1',
+              'path'          => '/var/log/btmp',
+              'create_mode'   => '0600',
+              'minsize'       => '1M',
+              'missingok'     => true,
+              'rotate_every'  => 'month',
+              'create'        => true,
+              'create_owner'  => 'root',
+              'create_group'  => 'utmp',
+              'rotate'        => '1',
+              'delaycompress' => 'undef',
+              'ifempty'       => 'undef',
             })
           end
         end
