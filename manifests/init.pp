2018f17e99d8bc06c31de6635cc3dda385ab5326
ncsutmf <tmfarwig@ncsu.edu>
2016-04-03 21:53:49 -0400

    Added ability to override default btmp and/or wtmp

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,14 +10,14 @@ class logrotate (
 
   include ::logrotate::install
   include ::logrotate::config
-  include ::logrotate::defaults
   include ::logrotate::rules
+  include ::logrotate::defaults
 
   anchor{'logrotate_begin':}->
   Class['::logrotate::install']->
   Class['::logrotate::config']->
-  Class['::logrotate::defaults']->
   Class['::logrotate::rules']->
+  Class['::logrotate::defaults']->
   anchor{'logrotate_end':}
 
 }
